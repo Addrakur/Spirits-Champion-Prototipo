14f2e2748cf1c18a8e3b40b5421c1bcf60992fd6
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2023-04-18 09:53:25 -0300

    Mudança no Level Manager
    Existe um loading entre o Forest Path e Forest 1

--- Spirits_Champion_Prototipo/Assets/Scripts/LevelManager.cs
+++ Spirits_Champion_Prototipo/Assets/Scripts/LevelManager.cs
@@ -7,19 +7,31 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private string nextScene; // Proxima cena a ser carregada
     [SerializeField] private string[] unloadScene; // Cenas antigas para dar unload
+    [SerializeField] private bool notAdditive;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player Box" && !SceneManager.GetSceneByName(nextScene).isLoaded) // Verifica se o colisor está colidindo com o player e se a próxima cena não está carregada
+        if(collision.gameObject.name == "Player Box") // Verifica se o colisor está colidindo com o player e se a próxima cena não está carregada
         {
-            SceneManager.LoadSceneAsync(nextScene,LoadSceneMode.Additive); // Carrega a próxima cena
-
-            for (int i = 0; i < unloadScene.Length; i++) 
+            if (notAdditive)
             {
-                if (SceneManager.GetSceneByName(unloadScene[i]).isLoaded) // Verifica se as cenas antigas estão carregadas
+                SceneManager.LoadScene(nextScene);
+            }
+            else
+            {
+            if (!SceneManager.GetSceneByName(nextScene).isLoaded)
+            {
+                SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive); // Carrega a próxima cena
+
+                for (int i = 0; i < unloadScene.Length; i++)
                 {
-                    SceneManager.UnloadSceneAsync(unloadScene[i]); // Da unload nas cenas antigas
+                    if (SceneManager.GetSceneByName(unloadScene[i]).isLoaded) // Verifica se as cenas antigas estão carregadas
+                    {
+                        SceneManager.UnloadSceneAsync(unloadScene[i]); // Da unload nas cenas antigas
+                    }
                 }
+            }
+
             }
         }
     }
