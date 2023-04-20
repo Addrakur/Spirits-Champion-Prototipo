6254f7ae87a61650ca09b27442eadd9fe4f90bc9
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2023-04-20 17:18:43 -0300

    Modificações no loading village - forest  path

--- Spirits_Champion_Prototipo/Assets/Scripts/GameManager.cs
+++ Spirits_Champion_Prototipo/Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private bool isBase;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (!isBase && !SceneManager.GetSceneByName("Base").isLoaded)
+        {
+            SceneManager.LoadScene("Base", LoadSceneMode.Additive);
+        }
+
     }
 
     // Update is called once per frame
